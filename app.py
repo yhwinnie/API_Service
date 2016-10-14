328616ade31c46535fdb649940cc1e5d675d05d4
Winnie Wen <wiwen@berkeley.edu>
2016-10-13 23:56:48 -0700

    Able to finish challenges (flask)

--- app.py
+++ app.py
@@ -5,17 +5,42 @@ import json
 
 app = Flask(__name__)
 
-info = ""
+
+pets = []
 
 @app.route('/hello')
 def index():
     return 'hello'
 
-@app.route('/pets', methods=['POST'])
-def store():
-    input_json = request.get_json()
 
-    return jsonify(input_json)
+# curl -H "Content-Type: application/json" -X POST -d '{"name":"Pluto","age":"3","species":"dog"}' http://0.0.0.0:5000/pets
+@app.route('/pets', methods=["POST"])
+def post_pets():
+    json_dict = request.get_json()
+    try:
+        name = json_dict['name']
+        age = json_dict['age']
+        species = json_dict['species']
+
+        data = {"name": str(name), "age": str(age), "species": str(species)}
+        pets.append(data)
+        return jsonify(data)
+    except:
+        print("ERROR")
+        return jsonify({"HTTP status": "400", "message": "JSON does not consist key"})
+
+
+@app.route('/pets', methods=["GET"])
+def get_pets():
+    return jsonify(pets)
+
+
+@app.route('/pets/<name>', methods=["GET"])
+def check_pet_exist(name):
+    for pet in pets:
+        if pet["name"] == name:
+            return jsonify(pet)
+    return jsonify({"HTTP status": "400", "message": "Key does not exist"})
 
 
 if __name__ == '__main__':
