76afb764bec8ef538133dc1f0f6e1869e0530c0d
Winnie Wen <wiwen@berkeley.edu>
2016-10-17 13:16:55 -0700

    Finsished challenges

--- app.py
+++ app.py
@@ -23,15 +23,53 @@ def post_pets():
         species = json_dict['species']
 
         data = {"name": str(name), "age": str(age), "species": str(species)}
+
+        for pet in pets:
+            if pet["name"] == str(name):
+                data = {"HTTP status": "409", "message": "Pet name already exists"}
+                return jsonify(data)
         pets.append(data)
         return jsonify(data)
-    except:
-        print("ERROR")
-        return jsonify({"HTTP status": "400", "message": "JSON does not consist key"})
+    except KeyError as error:
+        print(error.message + "key not find")
+        return jsonify({"HTTP status": "400", "message": error.message + " key not find"})
 
 
 @app.route('/pets', methods=["GET"])
 def get_pets():
+    return jsonify(pets)
+
+@app.route('/pets/<name>', methods=["PUT"])
+def put_request(name):
+    check_pet_exist(name)
+    json_dict = request.get_json()
+    for pet in pets:
+        if pet["name"] == name:
+            try:
+                if pet["name"] == str(name):
+                    pet["name"] = json_dict["name"]
+                    pet["age"] = json_dict["age"]
+                    pet["species"] = json_dict["species"]
+                    return jsonify(pets)
+                    break
+            except:
+                return jsonify({"HTTP status": "400", "message": "ERROR"})
+
+    return jsonify({"HTTP status": "400", "message": "Key: " + name + " does not exist"})
+
+
+@app.route('/pets/<name>', methods=["DELETE"])
+def delete_pet(name):
+    #check_pet_exist(name)
+    json_dict = request.get_json()
+    for pet in pets:
+        if pet["name"] == str(name):
+            try:
+                pets.pop(pet)
+                break
+            except:
+                return jsonify({"HTTP status": "400", "message": "Key: " + name + " does not exist"})
+
     return jsonify(pets)
 
 
@@ -40,7 +78,7 @@ def check_pet_exist(name):
     for pet in pets:
         if pet["name"] == name:
             return jsonify(pet)
-    return jsonify({"HTTP status": "400", "message": "Key does not exist"})
+    return jsonify({"HTTP status": "400", "message": "Key: " + name + " does not exist"})
 
 
 if __name__ == '__main__':
