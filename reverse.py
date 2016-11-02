87b714d288e463ce0f5c3801a729e9feb3720e50
Winnie Wen <wiwen@berkeley.edu>
2016-11-02 15:49:59 -0700

    reverse.py completed

---
+++ reverse.py
@@ -0,0 +1,23 @@
+import sys
+
+
+arguments = sys.argv[1:]
+
+
+def reverse_words():
+    reverse_list = []
+    i = len(arguments) - 1
+    while i >= 0:
+        reverse_list.append(arguments[i])
+
+        i -= 1
+    rtn = ', '.join(map(str, reverse_list))
+    return rtn
+
+
+
+
+
+
+if __name__ == '__main__':
+    print(reverse_words())
