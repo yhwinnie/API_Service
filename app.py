19f8a177a65b6907d98f6b9f3f4eb63fd9fd0d65
Winnie Wen <wiwen@berkeley.edu>
2017-01-26 15:49:19 -0800

    Added an import

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import unicode_literals
 from flask import Flask, render_template, request, redirect
 import re
 import random
--- twitter.py
+++ twitter.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import os
 print(os.environ)
 from flask import Flask, render_template, request, redirect
