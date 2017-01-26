105ee632c46f4ee377b39ada363893f3a13e4976
Winnie Wen <wiwen@berkeley.edu>
2017-01-26 13:57:53 -0800

    Added port

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def hello():
 
 if __name__ == '__main__':
     import sys
-    app.static_folder = 'static'
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
