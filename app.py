699b0b11a57dbb4765466fd13d2cb0c6220bf68e
Winnie Wen <wiwen@berkeley.edu>
2017-01-26 13:19:15 -0800

    Working site

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import markov
 import os
 
 tokens = markov.read(open("corpus_clean.txt").read())
-print(tokens)
+#print(tokens)
 sentence = markov.markov_model(tokens)
 
 app = Flask(__name__)
@@ -15,14 +15,25 @@ app = Flask(__name__)
 def main():
     sentence = markov.markov_model(tokens)
     #return ' '.join(sentence).replace(" ENDEND", '.')
-    return render_template("test.html", sentence=' '.join(sentence).replace(" ENDEND", '.'))
+    img_arr = ["https://espnfivethirtyeight.files.wordpress.com/2016/08/ap_16228136310156.jpg?quality=90&strip=all&w=575&ssl=1",
+    "http://cdn.history.com/sites/2/2013/11/obama_color.jpg","http://c7.nrostatic.com/sites/default/files/styles/original_image_with_cropping/public/uploaded/obama-lies-voter-id-many-countries-require-it.jpg?itok=IiyEm-f8",
+    "http://www.dailystormer.com/wp-content/uploads/2016/11/obama.jpg",
+    "http://www.fanabc.com/afanoromo/media/k2/items/cache/6d61dddd32fa4e7706fff4ee75eccb24_XL.jpg?t=1437743475",
+    "http://i2.cdn.cnn.com/cnnnext/dam/assets/161020125737-06-obama-presidency-super-169.jpg"]
+
+
+    img = random.choice(img_arr)
+    print(img)
+
+    return render_template("test.html", sentence=' '.join(sentence).replace(" ENDEND", '.'), img_src=img)
 
 @app.route('/hello')
 def hello():
-    return render_template("test.html", sentence=random_sentence)
+
+    return render_template("test.html", sentence=random_sentence, img_src=img)
 
 
 if __name__ == '__main__':
     import sys
-    #main()
+    app.static_folder = 'static'
     app.run(debug=True, host='0.0.0.0')
